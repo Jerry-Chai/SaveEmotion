cd3628d677df024dd44106ebeb9189ae92119a89
Jerrysheen <Jerryshin961115@gmail.com>
2023-08-09 21:39:52 +0800

    D: 蜗牛动画测试

--- SaveEmotion/Assets/Scripts/GameLogic/GridManager.cs
+++ SaveEmotion/Assets/Scripts/GameLogic/GridManager.cs
@@ -25,31 +25,31 @@ public class GridManager : Singleton<GridManager>
     {
         
 
-        var bricksInfo = GetComponent<BricksSetter>();
-//        bricksprefab = bricksInfo.bricksprefab;
-        upperLeft = bricksInfo.upperLeft;
-        lowerRight = bricksInfo.lowerRight;
-        // calculate Bounds:
-        bricksBounds = bricksprefab.GetComponent<MeshRenderer>().bounds;
-        Debug.Log(bricksBounds.size);
-        float width = Mathf.Abs(lowerRight.transform.position.x - upperLeft.transform.position.x);
-        float height = Mathf.Abs(lowerRight.transform.position.z - upperLeft.transform.position.z);
-        Debug.Log(width);
-        Debug.Log(height);
-
-        int xNum = Mathf.FloorToInt(width / bricksBounds.size.x);
-        int yNum = Mathf.FloorToInt(height / bricksBounds.size.z);
-        Debug.Log(xNum);
-        Debug.Log(yNum);
-        //for (int i = 0; i < xNum; i++)
-        //{
-        //    for (int j = 0; j < yNum; j++)
-        //    {
-        //        GameObject newBricks = Instantiate(bricksprefab);
-        //        newBricks.transform.parent = transform;
-        //        newBricks.transform.position = new Vector3(upperLeft.transform.position.x + bricksBounds.size.x * i, this.transform.position.y, upperLeft.transform.position.z - bricksBounds.size.z * j);
-        //    }
-        //}
+//         var bricksInfo = GetComponent<BricksSetter>();
+// //        bricksprefab = bricksInfo.bricksprefab;
+//         // upperLeft = bricksInfo.upperLeft;
+//         // lowerRight = bricksInfo.lowerRight;
+//         // calculate Bounds:
+//        // bricksBounds = bricksprefab.GetComponent<MeshRenderer>().bounds;
+//         Debug.Log(bricksBounds.size);
+//         float width = Mathf.Abs(lowerRight.transform.position.x - upperLeft.transform.position.x);
+//         float height = Mathf.Abs(lowerRight.transform.position.z - upperLeft.transform.position.z);
+//         Debug.Log(width);
+//         Debug.Log(height);
+//
+//         int xNum = Mathf.FloorToInt(width / bricksBounds.size.x);
+//         int yNum = Mathf.FloorToInt(height / bricksBounds.size.z);
+//         Debug.Log(xNum);
+//         Debug.Log(yNum);
+//         //for (int i = 0; i < xNum; i++)
+//         //{
+//         //    for (int j = 0; j < yNum; j++)
+//         //    {
+//         //        GameObject newBricks = Instantiate(bricksprefab);
+//         //        newBricks.transform.parent = transform;
+//         //        newBricks.transform.position = new Vector3(upperLeft.transform.position.x + bricksBounds.size.x * i, this.transform.position.y, upperLeft.transform.position.z - bricksBounds.size.z * j);
+//         //    }
+//         //}
 
     }
 
@@ -129,6 +129,7 @@ public class GridManager : Singleton<GridManager>
         int count = revertBlocksNum;
         while (count > 0) 
         {
+            
             count--;
         
         }
