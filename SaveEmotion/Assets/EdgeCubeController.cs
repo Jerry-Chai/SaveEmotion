33a32721bb8d55801f1a8f3efc68d84c704f05d4
Jerrysheen <Jerryshin961115@gmail.com>
2023-10-14 10:51:22 +0800

    D： 新资源

--- SaveEmotion/Assets/EdgeCubeController.cs
+++ SaveEmotion/Assets/EdgeCubeController.cs
@@ -99,7 +99,7 @@ public class EdgeCubeMovementController : MonoBehaviour
             var scale = new Vector3(UnityEngine.Random.Range(0.99f, 1.01f), 1, UnityEngine.Random.Range(0.99f, 1.01f));
             float scaleX = child.transform.localScale.x * scale.x;
             float scaleZ = child.transform.localScale.z * scale.z;
-            child.transform.localScale = new Vector3(scaleX, 7.0f, scaleZ);
+            child.transform.localScale = new Vector3(scaleX, UnityEngine.Random.Range(6, 8), scaleZ);
         }
         //for (int i = 0; i < transform.childCount; i++)
         //{
